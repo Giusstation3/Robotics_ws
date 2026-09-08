abdc5e9146cfde758340fea9d13fa3f4d7afb38c
Giusstation3 <giuseppevalencia28@gmail.com>
2026-09-08 16:25:11 -0600

    cambios finales

--- src/README.md
+++ src/README.md
@@ -1,3 +1,6 @@
 Giuseppe Valencia Carrillo
-...
+Prueba de velocity publisher y subscriber topicos 
+EL nodo publisher envía datos usando el topico /velocity usando mensajes de tipo float y el subscriptor escucha o se suscribe al topico para recibir los datos y mostrarlos
+Comandos utilizados: ros2 topic list, rqt_graph, ros2 topic info /velocity, ros2 node info /velocity_publisher python3 velocity publisher.py y python3 velocity_subscriber.py
+ningun problema encontrado durante el desarrollo
 
--- src/velocity_publisher.py
+++ src/velocity_publisher.py
@@ -2,17 +2,18 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32
 
-
+#Clase del publicador Node 
 class VelocityPublisher(Node):
 
     def __init__(self):
         super().__init__('velocity_publisher')
-
+	#Creamos el publicador con el topico /velocity con un mensaje float32 
         self.publisher_ = self.create_publisher(Float32,'/velocity',10)
         self.Vel = 0.0
         self.timer_ = self.create_timer(1.0,self.publish_velocity)
 
     def publish_velocity(self):
+	#instancia de objeto mensaje y guardamos el valor Vel en la data del mensaje 
         msg = Float32()
         msg.data = self.Vel
         self.publisher_.publish(msg)
